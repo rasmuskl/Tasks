47e416f201862c72bbb450d67a18667fe4d977d7
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2011-08-18 20:43:18 +0200

    Added a sample StoryQ test.

---
+++ Source/Tasks.Tests/SampleFixture.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using StoryQ;
+
+namespace Tasks.Tests
+{
+    public class SampleFixture : AbstractStoryQFixture
+    {
+        public override Feature InitStory()
+        {
+            return new Story("test")
+                .InOrderTo("test")
+                .AsA("test")
+                .IWant("test");
+        }
+
+        [Test]
+        public void Test()
+        {
+            Scenario(s =>
+                s.Given(Something)
+                .When(Something)
+                .Then(Something));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Scenario(s =>
+                s.Given(Something)
+                .When(Something)
+                .Then(Something));
+        }
+
+        private void Something()
+        {
+
+
+        }
+    }
+}
