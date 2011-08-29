4b46baca4c860f9c6804da4c62f80aa96ef7e78e
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2011-08-30 00:04:37 +0200

    Added UI context tabs.

---
+++ Source/Tasks.App/Controllers/ContextsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using Tasks.Read;
+
+namespace Tasks.App.Controllers
+{
+    public class ContextsController : Controller
+    {
+        public ActionResult MenuList()
+        {
+            Guid userId = ReadStorage.GetUserIdByEmail(User.Identity.Name);
+            return View(ReadStorage.GetContextsByUserId(userId));
+        }
+
+        public ActionResult Index(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
