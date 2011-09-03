6ae3efe24c98733bf77b526cf7e8141c56464a4a
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2011-09-03 10:36:45 +0200

    Added a simple lock to ReadStorage.

--- Source/Tasks.Read/ReadStorage.cs
+++ Source/Tasks.Read/ReadStorage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using EventStore;
 using StructureMap;
 using Tasks.Read.Models;
@@ -8,8 +9,11 @@ namespace Tasks.Read
 {
     public static class ReadStorage
     {
+        static ReaderWriterLockSlim _lock;
+
         static ReadStorage()
         {
+            _lock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
             Contexts = new Dictionary<Guid, List<ContextReadModel>>();
             Tasks = new Dictionary<Guid, List<TaskReadModel>>();
             Notes = new Dictionary<Guid, List<NoteReadModel>>();
@@ -17,14 +21,9 @@ namespace Tasks.Read
             PasswordHashes = new Dictionary<string, string>(StringComparer.InvariantCultureIgnoreCase);
         }
 
-        public static void HandleCommit(Commit commit)
-        {
-            ObjectFactory.GetInstance<EventHub>().HandleCommit(commit);
-        }
-
         internal static Dictionary<Guid, List<ContextReadModel>> Contexts { get; set; }
-        public static Dictionary<Guid, List<TaskReadModel>> Tasks { get; private set; }
-        public static Dictionary<Guid, List<NoteReadModel>> Notes { get; private set; }
+        internal static Dictionary<Guid, List<TaskReadModel>> Tasks { get; private set; }
+        internal static Dictionary<Guid, List<NoteReadModel>> Notes { get; private set; }
         public static Dictionary<string, Guid> RegisteredEmails { get; private set; }
         public static Dictionary<string, string> PasswordHashes { get; private set; }
 
@@ -38,10 +37,32 @@ namespace Tasks.Read
 
             var handler = ObjectFactory.GetInstance(genericHandlerType);
 
-            var handleMethod = handler.GetType().GetMethod("Handle", new [] { type });
-            var result = handleMethod.Invoke(handler, new[] {query});
+            try
+            {
+                _lock.EnterReadLock();
 
-            return (T)result;
+                var handleMethod = handler.GetType().GetMethod("Handle", new[] { type });
+                var result = handleMethod.Invoke(handler, new[] { query });
+
+                return (T)result;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+
+        public static void HandleCommit(Commit commit)
+        {
+            try
+            {
+                _lock.EnterWriteLock();
+                ObjectFactory.GetInstance<EventHub>().HandleCommit(commit);
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
         }
     }
 }
