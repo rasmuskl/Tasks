431ef8875453f0f54e13b747c4c43b34a7e770c6
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2011-09-03 22:32:51 +0200

    Added html stripping to note list on context page.

---
+++ Source/Tasks.App/Models/HtmlExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+using System.Web.Mvc;
+
+namespace Tasks.App.Models
+{
+    public static class HtmlExtensions
+    {
+        public static string StripHtml(this HtmlHelper helper, string htmlString)
+        {
+            htmlString = Regex.Replace(htmlString, @"</?[a-zA-Z]+\s*/?>", " ");
+            htmlString = Regex.Replace(htmlString, @"\s[2,]", " ");
+
+            return htmlString.Trim();
+        }
+
+    }
+}
