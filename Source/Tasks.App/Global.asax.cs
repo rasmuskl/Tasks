b950c1ba1a2cb78d91b185277faa717cd0aeec3a
RasmusKL@RKL-TP <RasmusKL@RKL-TP>
2011-09-08 09:51:43 +0200

    Added a preloader for IIS always running.

--- Source/Tasks.App/Global.asax.cs
+++ Source/Tasks.App/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using System.Web.Routing;
 using StructureMap;
@@ -13,54 +14,14 @@ namespace Tasks.App
 {
     public class MvcApplication : HttpApplication
     {
-        public static DateTime AppStarted { get; private set; }
+        public static DateTime AppStarted { get; set; }
 
         public void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
-
-            RegisterGlobalFilters(GlobalFilters.Filters);
-            RegisterRoutes(RouteTable.Routes);
-
-            ObjectFactory.Initialize(x =>
-            {
-                x.Scan(scanner =>
-                {
-                    scanner.TheCallingAssembly();
-                    scanner.AssemblyContainingType(typeof(ReadRegistry));
-                    scanner.AssemblyContainingType(typeof(WriteRegistry));
-
-                    scanner.LookForRegistries();
-
-                    scanner.WithDefaultConventions();
-                });
-            });
-
-            DependencyResolver.SetResolver(new StructureMapDependencyResolver(ObjectFactory.Container));
-
-            Storage.Init();
-
-            AppStarted = DateTime.Now;
+            new Preloader().Preload(new string[] {});
         }
 
-        private static void RegisterGlobalFilters(GlobalFilterCollection filters)
-        {
-            filters.Add(new HandleErrorAttribute());
-        }
-
-        private static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
-        }
-
-        private class StructureMapDependencyResolver : IDependencyResolver
+        public class StructureMapDependencyResolver : IDependencyResolver
         {
             private readonly IContainer _container;
 
@@ -83,6 +44,61 @@ namespace Tasks.App
             {
                 return ObjectFactory.GetAllInstances(serviceType).OfType<object>();
             }
+        }
+    }
+
+    public class Preloader : IProcessHostPreloadClient
+    {
+        public static bool _preloaded;
+
+        public void Preload(string[] parameters)
+        {
+            if (_preloaded)
+                return;
+
+            _preloaded = true;
+
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            ObjectFactory.Initialize(x =>
+            {
+                x.Scan(scanner =>
+                {
+                    scanner.TheCallingAssembly();
+                    scanner.AssemblyContainingType(typeof(ReadRegistry));
+                    scanner.AssemblyContainingType(typeof(WriteRegistry));
+
+                    scanner.LookForRegistries();
+
+                    scanner.WithDefaultConventions();
+                });
+            });
+
+            DependencyResolver.SetResolver(new MvcApplication.StructureMapDependencyResolver(ObjectFactory.Container));
+
+            Storage.Init();
+
+            MvcApplication.AppStarted = DateTime.Now;
+        }
+
+        private static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        private static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
         }
     }
 }
