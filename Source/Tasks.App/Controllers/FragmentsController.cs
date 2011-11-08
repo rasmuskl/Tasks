8df100f280d95b62197ec7c9bbda7efe3389127f
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2011-11-08 09:41:09 +0100

    Added basic create fragment page.

---
+++ Source/Tasks.App/Controllers/FragmentsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+using Tasks.Read;
+using Tasks.Read.Queries;
+using Tasks.Write;
+using Tasks.Write.Commands;
+
+namespace Tasks.App.Controllers
+{
+    public class FragmentsController : Controller
+    {
+        readonly CommandExecutor _executor;
+
+        public FragmentsController(CommandExecutor executor)
+        {
+            _executor = executor;
+        }
+
+        public ActionResult Index()
+        {
+            Guid userId = ReadStorage.Query(new QueryUserIdByEmail(User.Identity.Name));
+            var readModels = ReadStorage.Query(new QueryFragmentsByUserId(userId));
+            return View(readModels);
+        }
+
+        public ActionResult CreateFragment(string text)
+        {
+            if(string.IsNullOrWhiteSpace(text))
+            {
+                // TODO Better validation :-)
+                return View("Index");
+            }
+
+            Guid userId = ReadStorage.Query(new QueryUserIdByEmail(User.Identity.Name));
+            _executor.Execute(new CreateFragment(text, userId));
+
+            return RedirectToAction("Index");
+        }
+    }
+}
