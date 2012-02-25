edb5605769aac3ccf517b2f8f0397bb98b237e8b
Rasmus Kromann-Larsen (rasmus@kromann-larsen.dk) <Rasmus Kromann-Larsen (rasmus@kromann-larsen.dk)>
2012-02-25 11:30:54 +0100

    Added StoryQ.

---
+++ Source/Tasks.Tests/AbstractStoryQFixture.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using StoryQ;
+
+namespace Tasks.Tests
+{
+    public abstract class AbstractStoryQFixture
+    {
+        public abstract Feature InitStory();
+
+        public void Scenario(Func<Scenario, Outcome> scenarioFunc)
+        {
+            Scenario scenario = InitStory().WithScenario(StoryQHelper.ConvertTestMethodAsScenario());
+            scenarioFunc(scenario).Execute();
+        }
+
+        public void Scenario(string scenarioName, Func<Scenario, Outcome> scenarioFunc)
+        {
+            Scenario scenario = InitStory().WithScenario(scenarioName);
+            scenarioFunc(scenario).Execute();
+        }
+    }
+}
