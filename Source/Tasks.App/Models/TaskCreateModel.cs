22b4548b0bf69e116b959a94656baa3f63808820
Rasmus Kromann-Larsen <rasmus@kromann-larsen.dk>
2012-03-08 22:40:19 +0100

    Added ContextId to Create Task and removed Create Task view.

--- Source/Tasks.App/Models/TaskCreateModel.cs
+++ Source/Tasks.App/Models/TaskCreateModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tasks.App.Models
 {
@@ -6,5 +7,8 @@ namespace Tasks.App.Models
     {
         [Required]
         public string Title { get; set; }
+
+        [Required]
+        public Guid ContextId { get; set; }
     }
 }
